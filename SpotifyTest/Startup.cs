d2ab57149453c9b43d533c51adcc6febe3091506
Kristofer Nissbrandt <kristofer.nissbrandt@squeed.com>
2015-12-18 13:29:48 +0100

    First commit

---
+++ SpotifyTest/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SpotifyTest
+{
+    public class Startup
+    { 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()   
+        }
+    
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.UseStaticFiles();
+
+            //app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
